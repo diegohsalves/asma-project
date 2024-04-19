08b1ebeccdf3e25b8f61d75fb2e56d79c738e7f8
Diego Alves <diegohsalves@gmail.com>
2024-04-19 16:17:33 -0300

    Somente admin pode alterar/deletar outros usuários

--- src/main/java/br/com/asmaproject/config/security/SecurityConfig.java
+++ src/main/java/br/com/asmaproject/config/security/SecurityConfig.java
@@ -32,6 +32,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/registrar").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/usuario/atualizar").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/usuario").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
